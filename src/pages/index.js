e6b65a5fa146c5b0c51a916ac8a37aeb3a5608ee
MOE <moe.terani@gmail.com>
2019-03-17 15:31:44 +0100

    Initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,9 +3,13 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>
+      Learn to <br />design and code React apps
+    </h1>
+    <p>
+      Complete courses about the best tools and design systems. Prototype and
+      build apps with React and Swift.
+    </p>
     <Link to="/page-2/">Go to page 2</Link>
   </div>
 )
