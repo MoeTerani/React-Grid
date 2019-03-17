41947fc16826df8776e7c630d78296340d6548b5
MOE <moe.terani@gmail.com>
2019-03-17 17:16:33 +0100

    initial  styling  and normalizing

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Moe Terani',
   },
   plugins: ['gatsby-plugin-react-helmet'],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,14 +3,18 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>
-      Learn to <br />design and code React apps
-    </h1>
-    <p>
-      Complete courses about the best tools and design systems. Prototype and
-      build apps with React and Swift.
-    </p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <div className="Hero">
+      <div className="HeroGroup">
+        <h1>
+          Learn to <br />design and code React apps
+        </h1>
+        <p>
+          Complete courses about the best tools and design systems. Prototype
+          and build apps with React and Swift.
+        </p>
+        <Link to="/page-2/">Go to page 2</Link>
+      </div>
+    </div>
   </div>
 )
 
